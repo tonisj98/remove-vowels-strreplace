6bd1197ae952221d42e02ef4f42df1cc093aa853
Oscar Miras <oscar.miras@gmail.com>
2022-03-16 09:51:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const removeVowels = require('./remove-wowels');
+function removeVowels(frase) {
+    // Implementar código aquí
+   
+    // Obligatorio: utilizar una expresión regular y el método de string "replace": https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/replace
 
+    let resultado = "IMPLEMENTAME!!";
+
+    return resultado;
+}
 console.log(removeVowels('codewars')); // --> cdwrs
 console.log(removeVowels('holaholaaa')); // -->hlhl
