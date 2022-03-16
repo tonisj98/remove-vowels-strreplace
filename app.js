a7434b3e37072ac16f275a92ebfd8a61edafba86
tonisj98 <antonio.serjime@gmail.com>
2022-03-16 10:14:17 +0100

    hola

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function removeVowels(frase) {
     // Implementar código aquí
-   
     // Obligatorio: utilizar una expresión regular y el método de string "replace": https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/replace
 
     let resultado = "IMPLEMENTAME!!";
-
+    resultado = resultado.replace(resultado, frase)
+    resultado = resultado.replace(/[aeiou]/gi, '');
     return resultado;
 }
 console.log(removeVowels('codewars')); // --> cdwrs
